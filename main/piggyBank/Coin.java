95af1722d2c615576c66f31c011c5e9e904d3154
strengthofthemountain <strengthofthemountain@gmail.com>
2020-06-05 11:52:10 -0400

    added main and abstracts

--- main/piggyBank/Coin.java
+++ main/piggyBank/Coin.java
@@ -1,5 +1,23 @@
-package main.piggyBank;
+package piggyBank;
 
-public class Coin {
-    
+public abstract class Coin {
+    public abstract int getContent();
+    public abstract double getTotal();
+    public abstract double getValue();
+    public abstract void remove(double num);
+
+    //checks if the requested remove amount is greater than the existing total
+    public double check(double total){
+        double ttl=total;
+
+        if(ttl>this.getTotal()){
+            ttl-=this.getTotal();
+            this.remove(this.getContent());
+        }else{
+            double removed=ttl/this.getValue();
+            ttl-=removed*getValue();
+            this.remove(removed);
+        }
+        return ttl;
+    }
 }
